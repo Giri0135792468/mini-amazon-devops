eefee9e399a1164ebb30be831cc63ce014235696
giridhar <giridhargdv7@gmail.com>
2026-09-12 12:07:38 +0530

    fianl 2.0

--- terraform/modules/dynamodb/main.tf
+++ terraform/modules/dynamodb/main.tf
@@ -1,10 +1,10 @@
 resource "aws_dynamodb_table" "products" {
   name         = var.table_name
   billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "product_id"
+  hash_key     = "id"
 
   attribute {
-    name = "product_id"
+    name = "id"
     type = "S"
   }
 
