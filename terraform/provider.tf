109f21dbfdd1ed21f0b61f7b5f77cedf88f3c4a0
giridhar <giridhargdv7@gmail.com>
2026-09-13 12:27:26 +0530

    final 13.0

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -18,3 +18,16 @@ data "aws_eks_cluster_auth" "main" {
   ]
 }
 
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.main.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.main.token
+}
+
+provider "helm" {
+  kubernetes = {
+    host                   = data.aws_eks_cluster.main.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.main.token
+  }
+}
